2268d42c137847c4850bbbfaaa756d4d4efe0291
ericcialdelli <ericcialdelli@0f544f79-e0f4-099d-537f-5924260e9aa1>
2012-11-16 17:11:42 +0000

    20121116

--- trunk/SIIF/src/ar/com/siif/fachada/IReportesPorProductoFachada.java
+++ trunk/SIIF/src/ar/com/siif/fachada/IReportesPorProductoFachada.java
@@ -4,6 +4,8 @@ import ar.com.siif.negocio.exception.NegocioException;
 
 public interface IReportesPorProductoFachada {
 
-	public byte[] generarReporteVolumenPorAnioForestal(String path, String volumen)throws NegocioException;	
+	public byte[] generarReporteVolumenPorAnioForestal(String path, String volumen)throws NegocioException;
+	
+	public byte[] generarReporteVolumenPorProductorEntreFechas(String path, String volumen, String productor, String fechaDesde, String fechaHasta)throws NegocioException;
 	
 }
