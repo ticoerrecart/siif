53358b290232bf7bd37c44e05976be2e6b8db58f
ticoerrecart@gmail.com <ticoerrecart@gmail.com@0f544f79-e0f4-099d-537f-5924260e9aa1>
2013-06-11 14:07:44 +0000

    validación de Aforo

--- trunk/SIIF/src/ar/com/siif/struts/actions/AforoAction.java
+++ trunk/SIIF/src/ar/com/siif/struts/actions/AforoAction.java
@@ -11,20 +11,14 @@ import org.apache.struts.action.ActionMapping;
 import org.springframework.web.context.WebApplicationContext;
 
 import ar.com.siif.dto.AforoDTO;
-import ar.com.siif.dto.TipoProductoDTO;
 import ar.com.siif.dto.TipoProductoForestalDTO;
 import ar.com.siif.dto.UsuarioDTO;
 import ar.com.siif.fachada.IAforoFachada;
 import ar.com.siif.fachada.IEntidadFachada;
 import ar.com.siif.fachada.IRolFachada;
 import ar.com.siif.fachada.ITipoProductoForestalFachada;
-import ar.com.siif.negocio.Aforo;
-import ar.com.siif.negocio.TipoProducto;
-import ar.com.siif.negocio.Usuario;
 import ar.com.siif.negocio.exception.NegocioException;
-
 import ar.com.siif.struts.actions.forms.AforoForm;
-import ar.com.siif.struts.actions.forms.TipoProductoForestalForm;
 import ar.com.siif.struts.utils.Validator;
 import ar.com.siif.utils.Constantes;
 import ar.com.siif.utils.MyLogger;
@@ -32,47 +26,56 @@ import ar.com.siif.utils.MyLogger;
 public class AforoAction extends ValidadorAction {
 
 	@SuppressWarnings("unchecked")
-	public ActionForward cargarAltaAforo(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public ActionForward cargarAltaAforo(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 
 		String strForward = "exitoCargarAltaAforo";
 
 		try {
-			UsuarioDTO usuario = (UsuarioDTO)request.getSession().getAttribute(Constantes.USER_LABEL_SESSION);			
-			WebApplicationContext ctx = getWebApplicationContext();			
-			
-			IRolFachada rolFachada = (IRolFachada) ctx.getBean("rolFachada");
-			//rolFachada.verificarMenu(Constantes.ALTA_AFORO_MENU,usuario.getRol());
-			
-			ITipoProductoForestalFachada tipoProductoFachada = 
-					(ITipoProductoForestalFachada) ctx.getBean("tipoProductoForestalFachada");
-			IEntidadFachada entidadFachada = (IEntidadFachada) ctx.getBean("entidadFachada");
-			
-			List<TipoProductoForestalDTO> tiposProducto = tipoProductoFachada.recuperarTiposProductoForestalDTO();
+			UsuarioDTO usuario = (UsuarioDTO) request.getSession()
+					.getAttribute(Constantes.USER_LABEL_SESSION);
+			WebApplicationContext ctx = getWebApplicationContext();
 
-			request.setAttribute("estadosProducto", tipoProductoFachada.getEstadosProductos());
+			IRolFachada rolFachada = (IRolFachada) ctx.getBean("rolFachada");
+			// rolFachada.verificarMenu(Constantes.ALTA_AFORO_MENU,usuario.getRol());
+
+			ITipoProductoForestalFachada tipoProductoFachada = (ITipoProductoForestalFachada) ctx
+					.getBean("tipoProductoForestalFachada");
+			IEntidadFachada entidadFachada = (IEntidadFachada) ctx
+					.getBean("entidadFachada");
+
+			List<TipoProductoForestalDTO> tiposProducto = tipoProductoFachada
+					.recuperarTiposProductoForestalDTO();
+
+			request.setAttribute("estadosProducto",
+					tipoProductoFachada.getEstadosProductos());
 			request.setAttribute("tiposProducto", tiposProducto);
-			request.setAttribute("tiposDeEntidad", entidadFachada.getTiposDeEntidadProductores());
-			request.setAttribute("exitoGrabado", request.getAttribute("exitoGrabado"));
+			request.setAttribute("tiposDeEntidad",
+					entidadFachada.getTiposDeEntidadProductores());
+			request.setAttribute("exitoGrabado",
+					request.getAttribute("exitoGrabado"));
 
 		} catch (Throwable t) {
 			MyLogger.logError(t);
 			request.setAttribute("error", "Error Inesperado");
 			strForward = "error";
-		}			
+		}
 
 		return mapping.findForward(strForward);
 	}
 
 	@SuppressWarnings("unchecked")
 	public ActionForward altaAforo(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) throws Exception {
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
 		String strForward = "exitoAltaAforo";
 
 		try {
 
 			WebApplicationContext ctx = getWebApplicationContext();
-			IAforoFachada aforoFachada = (IAforoFachada) ctx.getBean("aforoFachada");
+			IAforoFachada aforoFachada = (IAforoFachada) ctx
+					.getBean("aforoFachada");
 
 			AforoForm aforoForm = (AforoForm) form;
 			aforoFachada.altaAforo(aforoForm.getAforoDTO());
@@ -92,18 +95,21 @@ public class AforoAction extends ValidadorAction {
 	}
 
 	@SuppressWarnings("unchecked")
-	public ActionForward cargarModificacionAforos(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public ActionForward cargarModificacionAforos(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		String strForward = "exitoCargarModificacionAforos";
 
 		try {
-			UsuarioDTO usuario = (UsuarioDTO)request.getSession().getAttribute(Constantes.USER_LABEL_SESSION);			
+			UsuarioDTO usuario = (UsuarioDTO) request.getSession()
+					.getAttribute(Constantes.USER_LABEL_SESSION);
 			WebApplicationContext ctx = getWebApplicationContext();
-			
+
 			IRolFachada rolFachada = (IRolFachada) ctx.getBean("rolFachada");
-			//rolFachada.verificarMenu(Constantes.MODIFICACION_AFORO_MENU,usuario.getRol());
-			
-			IAforoFachada aforoFachada = (IAforoFachada) ctx.getBean("aforoFachada");
+			// rolFachada.verificarMenu(Constantes.MODIFICACION_AFORO_MENU,usuario.getRol());
+
+			IAforoFachada aforoFachada = (IAforoFachada) ctx
+					.getBean("aforoFachada");
 
 			List<AforoDTO> aforos = aforoFachada.recuperarAforosDTO();
 			request.setAttribute("aforos", aforos);
@@ -119,55 +125,66 @@ public class AforoAction extends ValidadorAction {
 
 	@SuppressWarnings("unchecked")
 	public ActionForward recuperarAforo(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) throws Exception {
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
 		String strForward = "exitoRecuperarAforo";
 
 		try {
-			UsuarioDTO usuario = (UsuarioDTO)request.getSession().getAttribute(Constantes.USER_LABEL_SESSION);			
-			WebApplicationContext ctx = getWebApplicationContext();			
-			
+			UsuarioDTO usuario = (UsuarioDTO) request.getSession()
+					.getAttribute(Constantes.USER_LABEL_SESSION);
+			WebApplicationContext ctx = getWebApplicationContext();
+
 			IRolFachada rolFachada = (IRolFachada) ctx.getBean("rolFachada");
-			//rolFachada.verificarMenu(Constantes.MODIFICACION_AFORO_MENU,usuario.getRol());
-			
-			IEntidadFachada entidadFachada = (IEntidadFachada) ctx.getBean("entidadFachada");			
-			IAforoFachada aforoFachada = (IAforoFachada) ctx.getBean("aforoFachada");
-			ITipoProductoForestalFachada tipoProductoFachada = 
-				(ITipoProductoForestalFachada) ctx.getBean("tipoProductoForestalFachada");
-			
+			// rolFachada.verificarMenu(Constantes.MODIFICACION_AFORO_MENU,usuario.getRol());
+
+			IEntidadFachada entidadFachada = (IEntidadFachada) ctx
+					.getBean("entidadFachada");
+			IAforoFachada aforoFachada = (IAforoFachada) ctx
+					.getBean("aforoFachada");
+			ITipoProductoForestalFachada tipoProductoFachada = (ITipoProductoForestalFachada) ctx
+					.getBean("tipoProductoForestalFachada");
+
 			String id = request.getParameter("id");
 
-			AforoDTO aforo = aforoFachada.recuperarAforoDTO(new Long(id).longValue());
-			List<TipoProductoForestalDTO> tiposProducto = tipoProductoFachada.recuperarTiposProductoForestalDTO();
+			AforoDTO aforo = aforoFachada.recuperarAforoDTO(new Long(id)
+					.longValue());
+			List<TipoProductoForestalDTO> tiposProducto = tipoProductoFachada
+					.recuperarTiposProductoForestalDTO();
 
-			request.setAttribute("tiposDeEntidad", entidadFachada.getTiposDeEntidadProductores());
-			request.setAttribute("estadosProducto", tipoProductoFachada.getEstadosProductos());
+			request.setAttribute("tiposDeEntidad",
+					entidadFachada.getTiposDeEntidadProductores());
+			request.setAttribute("estadosProducto",
+					tipoProductoFachada.getEstadosProductos());
 			request.setAttribute("tiposProducto", tiposProducto);
 			request.getSession().setAttribute("aforoParam", aforo);
-			
+
 		} catch (Throwable t) {
 			MyLogger.logError(t);
 			request.setAttribute("error", "Error Inesperado");
 			strForward = "bloqueError";
-		}			
+		}
 
 		return mapping.findForward(strForward);
 	}
 
 	@SuppressWarnings("unchecked")
-	public ActionForward modificacionAforo(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public ActionForward modificacionAforo(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		String strForward = "exitoModificacionAforo";
 
 		try {
 
 			WebApplicationContext ctx = getWebApplicationContext();
-			IAforoFachada aforoFachada = (IAforoFachada) ctx.getBean("aforoFachada");
+			IAforoFachada aforoFachada = (IAforoFachada) ctx
+					.getBean("aforoFachada");
 
 			AforoForm aforoForm = (AforoForm) form;
-			
+
 			aforoFachada.modificacionAforo(aforoForm.getAforoDTO());
 
-			request.setAttribute("exitoGrabado", Constantes.EXITO_MODIFICACION_AFORO);
+			request.setAttribute("exitoGrabado",
+					Constantes.EXITO_MODIFICACION_AFORO);
 
 		} catch (NegocioException ne) {
 			request.setAttribute("error", ne.getMessage());
@@ -182,24 +199,29 @@ public class AforoAction extends ValidadorAction {
 	}
 
 	public boolean validarAforoForm(StringBuffer error, ActionForm form) {
-		try{
+		try {
 			AforoForm aforoForm = (AforoForm) form;
 			WebApplicationContext ctx = getWebApplicationContext();
-			IAforoFachada aforoFachada = (IAforoFachada) ctx.getBean("aforoFachada");
+			IAforoFachada aforoFachada = (IAforoFachada) ctx
+					.getBean("aforoFachada");
 
-			boolean existe = aforoFachada.existeAforo(aforoForm.getAforoDTO(),
-					new Long(aforoForm.getAforoDTO().getTipoProducto().getId()));
-	
-			if (existe) {
-				Validator.addErrorXML(error, Constantes.EXISTE_AFORO);
+			boolean ok = aforoForm.validar(error);
+			boolean existe = true;
+			if (ok) {
+				existe = aforoFachada.existeAforo(aforoForm.getAforoDTO(),
+						new Long(aforoForm.getAforoDTO().getTipoProducto()
+								.getId()));
+				if (existe) {
+					Validator.addErrorXML(error, Constantes.EXISTE_AFORO);
+				}
 			}
-	
-			return !existe && aforoForm.validar(error);
-			
+
+			return !existe && ok;
+
 		} catch (Throwable t) {
 			MyLogger.logError(t);
 			Validator.addErrorXML(error, "Error Inesperado");
 			return false;
-		}		
+		}
 	}
 }
