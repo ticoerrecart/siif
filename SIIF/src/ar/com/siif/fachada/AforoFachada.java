82b56bb321438d423350faa1e1168c79ff155374
ticoerrecart@gmail.com <ticoerrecart@gmail.com@0f544f79-e0f4-099d-537f-5924260e9aa1>
2014-04-01 14:18:20 +0000

    Modificacion Aforo Nuevo

--- SIIF/src/ar/com/siif/fachada/AforoFachada.java
+++ SIIF/src/ar/com/siif/fachada/AforoFachada.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ar.com.siif.dao.AforoDAO;
-import ar.com.siif.dao.TipoProductoForestalDAO;
 import ar.com.siif.dto.AforoDTO;
+import ar.com.siif.dto.AforoNuevoDTO;
 import ar.com.siif.negocio.Aforo;
-import ar.com.siif.negocio.TipoProducto;
+import ar.com.siif.negocio.AforoNuevo;
 import ar.com.siif.negocio.TipoProductoForestal;
-import ar.com.siif.negocio.exception.DataBaseException;
 import ar.com.siif.negocio.exception.NegocioException;
 import ar.com.siif.providers.ProviderDTO;
 import ar.com.siif.providers.ProviderDominio;
@@ -23,7 +22,8 @@ public class AforoFachada implements IAforoFachada {
 	public AforoFachada() {
 	}
 
-	public AforoFachada(AforoDAO aforoDAO, ITipoProductoForestalFachada pTipoProductoFachada) {
+	public AforoFachada(AforoDAO aforoDAO,
+			ITipoProductoForestalFachada pTipoProductoFachada) {
 
 		this.aforoDAO = aforoDAO;
 		this.tipoProductoFachada = pTipoProductoFachada;
@@ -31,64 +31,104 @@ public class AforoFachada implements IAforoFachada {
 
 	public void altaAforo(AforoDTO aforo) throws NegocioException {
 
-		TipoProductoForestal tipoProducto = tipoProductoFachada.recuperarTipoProductoForestal(aforo.getTipoProducto().getId());
-		aforoDAO.altaAforo(ProviderDominio.getAforo(aforo,tipoProducto));				
+		TipoProductoForestal tipoProducto = tipoProductoFachada
+				.recuperarTipoProductoForestal(aforo.getTipoProducto().getId());
+		aforoDAO.altaAforo(ProviderDominio.getAforo(aforo, tipoProducto));
 	}
 
-	public List<Aforo> recuperarAforos(){
+	public List<Aforo> recuperarAforos() {
 
-		return aforoDAO.recuperarAforos();		
+		return aforoDAO.recuperarAforos();
 	}
 
-	public Aforo recuperarAforo(Long id){
+	public Aforo recuperarAforo(Long id) {
 
-		return aforoDAO.recuperarAforo(id);		
+		return aforoDAO.recuperarAforo(id);
+	}
+
+	public AforoNuevo recuperarAforoNuevo(Long id) {
+		return aforoDAO.recuperarAforoNuevo(id);
 	}
 
 	public void modificacionAforo(AforoDTO aforoDTO) throws NegocioException {
 
 		Aforo aforo = aforoDAO.recuperarAforo(aforoDTO.getId());
-		TipoProductoForestal tipoProducto = tipoProductoFachada.recuperarTipoProductoForestal(aforoDTO.getTipoProducto().getId());
-		
-		aforoDAO.modificacionAforo(ProviderDominio.getAforo(aforo, aforoDTO, tipoProducto));			
+		TipoProductoForestal tipoProducto = tipoProductoFachada
+				.recuperarTipoProductoForestal(aforoDTO.getTipoProducto()
+						.getId());
+
+		aforoDAO.modificacionAforo(ProviderDominio.getAforo(aforo, aforoDTO,
+				tipoProducto));
 	}
 
-	public boolean existeAforo(AforoDTO aforo, Long idTipoProducto){
+	public void modificacionAforoNuevo(AforoNuevoDTO aforoDTO) {
+		AforoNuevo aforoNuevo = aforoDAO.recuperarAforoNuevo(aforoDTO.getId());
+		if (aforoDTO.getMonto().doubleValue() > 0.0) {
+			aforoNuevo.setMonto(aforoDTO.getMonto());
+		} else {
+			if (aforoDTO.getPorcentaje().intValue() > 0) {
 
-		return aforoDAO.existeAforo(aforo, idTipoProducto);	
+				aforoNuevo.setPorcentaje(aforoDTO.getPorcentaje());
+			}
+		}
+
 	}
-	
-	public String getValor(String estado, Long idTipoProducto, Long idProdForestal)throws NegocioException{
 
-		try{
-			Aforo aforo = aforoDAO.getAforo(estado,idTipoProducto,idProdForestal);
-			
-			if(aforo != null){
+	public boolean existeAforo(AforoDTO aforo, Long idTipoProducto) {
+
+		return aforoDAO.existeAforo(aforo, idTipoProducto);
+	}
+
+	public String getValor(String estado, Long idTipoProducto,
+			Long idProdForestal) throws NegocioException {
+
+		try {
+			Aforo aforo = aforoDAO.getAforo(estado, idTipoProducto,
+					idProdForestal);
+
+			if (aforo != null) {
 				return String.valueOf(aforo.getValorAforo());
-			}	
+			}
 		} catch (Throwable t) {
 			MyLogger.logError(t);
 			throw new NegocioException("Error Inesperado");
-		}			
-			
+		}
+
 		return null;
 	}
-	
-	public List<AforoDTO> recuperarAforosDTO(){
-		
+
+	public List<AforoDTO> recuperarAforosDTO() {
+
 		List<AforoDTO> listaAforosDTO = new ArrayList<AforoDTO>();
 		List<Aforo> listaAforos = aforoDAO.recuperarAforos();
-		
+
 		for (Aforo aforo : listaAforos) {
 			listaAforosDTO.add(ProviderDTO.getAforoDTO(aforo));
 		}
-		
-		return listaAforosDTO;			
-	}	
-	
-	public AforoDTO recuperarAforoDTO(Long id){
+
+		return listaAforosDTO;
+	}
+
+	public List<AforoNuevoDTO> recuperarAforosNuevosDTO() {
+
+		List<AforoNuevoDTO> listaAforosDTO = new ArrayList<AforoNuevoDTO>();
+		List<AforoNuevo> listaAforos = aforoDAO.recuperarAforosNuevos();
+
+		for (AforoNuevo aforo : listaAforos) {
+			listaAforosDTO.add(ProviderDTO.getAforoNuevoDTO(aforo));
+		}
+
+		return listaAforosDTO;
+	}
+
+	public AforoDTO recuperarAforoDTO(Long id) {
 
 		Aforo aforo = aforoDAO.recuperarAforo(id);
-		return ProviderDTO.getAforoDTO(aforo);			
-	}	
+		return ProviderDTO.getAforoDTO(aforo);
+	}
+
+	public AforoNuevoDTO recuperarAforoNuevoDTO(Long id) {
+		AforoNuevo aforo = aforoDAO.recuperarAforoNuevo(id);
+		return ProviderDTO.getAforoNuevoDTO(aforo);
+	}
 }
